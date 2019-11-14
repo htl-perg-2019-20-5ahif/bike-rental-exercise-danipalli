a7090bfbcbf2627e566f8cd6f9a3aec2f19fada7
Daniel <20150107@students.htl-perg.ac.at>
2019-11-14 15:17:34 +0100

    fix some bugs

--- BikeRentalService/BikeRentalServiceWebAPI/Startup.cs
+++ BikeRentalService/BikeRentalServiceWebAPI/Startup.cs
@@ -6,8 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace BikeRentalServiceWebAPI
+namespace BikeRentalServiceWebAPI // http://bike-rental-api.azurewebsites.net/
 {
     public class Startup
     {
@@ -29,11 +30,11 @@ namespace BikeRentalServiceWebAPI
             services.AddControllers().AddNewtonsoftJson(settings =>
                 settings.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
-            //// Register the Swagger generator, defining 1 or more Swagger documents
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "BikeRentalAPI", Version = "v1" });
-            //});
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo  { Title = "BikeRentalAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,15 +45,15 @@ namespace BikeRentalServiceWebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            //// Enable middleware to serve generated Swagger as a JSON endpoint.
-            //app.UseSwagger();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            //// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            //// specifying the Swagger JSON endpoint.
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BikeRentalAPI");
-            //});
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BikeRentalAPI");
+            });
 
             app.UseRouting();
 
